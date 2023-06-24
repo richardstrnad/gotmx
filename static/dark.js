196aceb3b1415d4935cb0fab790fe2494d6f51d1
Richard Strnad <git@strnad.ch>
2023-06-24 08:11:39 +0200

    update

--- static/dark.js
+++ static/dark.js
@@ -1,17 +1,39 @@
-const toggleBtn = document.getElementById("toggle-btn");
-let darkMode = localStorage.getItem("dark-mode");
-
 const enableDarkMode = () => {
   document.documentElement.classList.add("dark");
-  toggleBtn.classList.remove("dark-mode-toggle");
-  localStorage.setItem("dark-mode", "enabled");
+  setCookie("dark-mode", "enabled", 365);
+  const toggleBtn = document.getElementById("toggle-btn");
+  toggleBtn.checked = true;
 };
 
 const disableDarkMode = () => {
   document.documentElement.classList.remove("dark");
-  toggleBtn.classList.add("dark-mode-toggle");
-  localStorage.setItem("dark-mode", "disabled");
+  setCookie("dark-mode", "disabled", 365);
 };
+
+const setCookie = (cname, cvalue, exdays) => {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Strict";
+}
+
+const getCookie = (cname) => {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+let darkMode = getCookie("dark-mode");
+console.log(darkMode)
 
 if (darkMode === "enabled") {
   enableDarkMode();
@@ -23,13 +45,15 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
   }
 }
 
-document.body.addEventListener("click", (element) => {
+document.body.addEventListener("change", (element) => {
   if (element.target.id === "toggle-btn") {
-    darkMode = localStorage.getItem("dark-mode");
+    darkMode = getCookie("dark-mode");
     if (darkMode === "disabled") {
       enableDarkMode();
+      element.target.checked = true;
     } else {
       disableDarkMode();
+      element.target.checked = false;
     }
   }
 });
